157549caac143afdb3a3fd688b149858ad3d79f2
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-05 23:27:55 +0200

    Added SectionSkills components to PersonelHomepage

--- src/feauters/personalHomepage/index.js
+++ src/feauters/personalHomepage/index.js
@@ -1,13 +1,17 @@
 import { Container } from "./Container/styled";
 import { Header } from "./Header";
+import { SectionSkills } from "./SectionSkills";
+import { skills, toLearn } from "./SectionSkills/skills";
 
 
 export const PersonalHomepage = () => {
     return (
-<>
-<Container>
-<Header />
-</Container>
-</>
+        <>
+            <Container>
+                <Header />
+                <SectionSkills title="My skillset includes" content={skills} />
+                <SectionSkills title="What I want to learn next" content={toLearn} />
+            </Container>
+        </>
     )
 };

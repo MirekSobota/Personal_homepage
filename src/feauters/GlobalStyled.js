852bab0f41fe689a956cf75d2a8973a867f80c2e
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-05 16:20:46 +0200

    Added body styled

--- src/feauters/GlobalStyled.js
+++ src/feauters/GlobalStyled.js
@@ -7,4 +7,13 @@ html {
 
 *, ::after, ::before {
   box-sizing: inherit;
-}`;
+}
+
+body{
+  margin:115px auto;
+  font-family:"Inter",sans-serif;
+  color:${({ theme }) => theme.primary.text};
+  background-color: ${({ theme }) => theme.primary.background}
+}
+`;
+

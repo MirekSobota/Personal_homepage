ffd95185c77a9807b4b396d7d0f949a3c5c43442
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-23 21:31:51 +0200

    Added ThemeSwitch button

--- src/common/ThemeSwitcher/index.js
+++ src/common/ThemeSwitcher/index.js
@@ -1,19 +1,24 @@
-import { Wrapper, Button, Sun, Bright, Background, Toggle } from "./styled";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Button,
+  Sun,
+  Background,
+  Toggle,
+} from "./styled";
 
 export const ThemeSwitcher = () => {
-
-    return (
-        <Wrapper>
-            <Button>
-                {/* <Sun /> */}
-                {/* <Bright /> */}
-
-                {/* <Background> */}
-                    {/* <Toggle> */}
-                        <Bright />
-                    {/* </Toggle> */}
-                {/* </Background> */}
-            </Button>
-        </Wrapper>
-    )
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Dark mode off</Title>
+        <Button>
+          <Background />
+          <Toggle />
+          <Sun />
+        </Button>
+      </Wrapper>
+    </Container>
+  );
 };
--- src/common/ThemeSwitcher/styled.js
+++ src/common/ThemeSwitcher/styled.js
@@ -1,35 +1,59 @@
 import styled from "styled-components";
 import { ReactComponent as SunIcon } from "./assets/lightTheme/sunIcon.svg";
-import { ReactComponent as BrightnessIcon } from "./assets/lightTheme/brightness.svg";
-import { ReactComponent as BackgroundIcon} from "./assets/lightTheme/background.svg";
-import { ReactComponent as ToggleIcon} from "./assets/lightTheme/toggle.svg";
+import { ReactComponent as BackgroundIcon } from "./assets/lightTheme/background.svg";
+import { ReactComponent as ToggleIcon } from "./assets/lightTheme/toggle.svg";
+
+export const Container = styled.div`
+  margin: 60px auto 0;
+  display: flex;
+  justify-content: flex-end;
+`;
 
 export const Wrapper = styled.div`
-    margin: 60px auto 0;
-    display: flex;
-    justify-content: flex-end;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
 `;
 
+export const Title = styled.p`
+font-size: 12px;
+font-style: normal;
+font-weight: 700;
+line-height: 1.3;
+text-transform: uppercase;
+`;
 export const Button = styled.button`
-
-`;
-
-export const Sun = styled(SunIcon)`
-background: ${({ theme }) => theme.primaryBlue.background};
-color: ${({ theme }) => theme.primaryBlue.text};
-`;
-
-export const Bright = styled(BrightnessIcon)`
-background: ${({ theme }) => theme.primaryBlue.background};
-color: ${({ theme }) => theme.primaryBlue.text};
+ background-color: transparent;
+  border: none;
+  cursor: pointer;
+  width: 48px;
+  position: relative;
 `;
 
 export const Background = styled(BackgroundIcon)`
-background: ${({ theme }) => theme.divider.background};
-color: ${({ theme }) => theme.header.text};
+position: absolute;
+  width: 48px;
+  height: 26px;
+  transform: translate(-50%, -50%);
 `;
 
-export const  Toggle = styled(ToggleIcon)`
-background: ${({ theme }) => theme.divider.background};
-color: ${({ theme }) => theme.header.text};
+export const Toggle = styled(ToggleIcon)`
+position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 20px;
+  height: 20px;
+  transform: translate(-100%, -50%);
 `;
+
+export const Sun = styled(SunIcon)`
+   position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 14px;
+  height: 14px;
+  transform: translate(-120%, -50%);
+`;
+
+

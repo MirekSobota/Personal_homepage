72f1020ed0a76a60c52c79e93875db23b5621002
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-23 21:32:32 +0200

    Added slice and saga

---
+++ src/personalPageSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const personalPageSlice = createSlice({
+  name: "personalPage",
+  initialState: {
+    theme: lightTheme,
+  },
+  reducers: {
+    toggleTheme: (state)=> {
+        state.lightTheme = !state.darkTheme;
+    },
+  },
+});
+
+export const {
+    toggleTheme,
+} = personalPageSlice.actions;
+
+export const selectPersonalPageState = (state) => state.personalPage;
+export const selectTheme = (state) => selectPersonalPageState(state).theme;
+
+export default personalPageSlice.reducer;

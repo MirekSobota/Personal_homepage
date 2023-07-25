854ee7a93b982a38a4438bfefd26956cc03cef41
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-25 13:26:55 +0200

    Added margin

--- src/feauters/personalHomepage/Portfolio/styled.js
+++ src/feauters/personalHomepage/Portfolio/styled.js
@@ -5,8 +5,6 @@ import { ReactComponent as GitHubIcon } from "./github.svg";
 export const Container = styled.section`
 display: grid;
 justify-items: center;
-
-
 `;
 
 export const GitHub = styled(GitHubIcon)`
@@ -18,6 +16,7 @@ font-style: normal;
 font-weight: 900;
 line-height: normal;
 letter-spacing: 1.5px;
+margin: 12px 0 0 0;
 `;
 
 export const SubTitle = styled.p`
@@ -26,4 +25,5 @@ font-style: normal;
 font-weight: 400;
 line-height: 1.4; 
 letter-spacing: 1px;
+margin: 8px 0 0 0;
 `; 

bdda308cf691c0d466e1ff746640f79333d63594
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-01 21:38:40 +0200

    Added store and saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,5 +1,9 @@
 import { all } from "redux-saga/effects";
+import {watchFetchHomePage} from "../src/feauters/personalHomepage/homePageSaga";
 
 export default function* rootSaga() {
-  yield all([]);
+  yield all([
+    watchFetchHomePage(),
+  ]);
 }
+
--- src/store.js
+++ src/store.js
@@ -2,17 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 
 import personalPageReducer from "./personalPageSlice";
+import homePageReducer from "../src/feauters/personalHomepage/homePageSlice";
 
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducers: {
-    personalPage: personalPageReducer,
+  reducer: {
+    personalPage: personalPageReducer, 
+    homePage: homePageReducer,
   },
   middleware: [sagaMiddleware],
 });
 
 sagaMiddleware.run(rootSaga);
 export default store;
+
+

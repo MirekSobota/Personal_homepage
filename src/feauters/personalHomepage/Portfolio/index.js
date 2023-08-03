fda3cb1d392367394c0db78da4c2ca430cf28b62
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-03 19:39:09 +0200

    Added github repos to portfolio

--- src/feauters/personalHomepage/Portfolio/index.js
+++ src/feauters/personalHomepage/Portfolio/index.js
@@ -1,13 +1,36 @@
-import { Container, GitHub, Header, SubTitle } from "./styled";
+import { useEffect } from "react";
+import { Container, GitHub, Header, SubTitle, StyledList, StyledListItem, SubHeader, StyledLink, Paragraph, Description } from "./styled";
+import { useDispatch, useSelector } from "react-redux";
 
+import { fetchHomePageLoad, selectReposState } from "../homePageSlice";
 
 export const Portfolio = () => {
+  const dispatch = useDispatch();
+  const repositories = useSelector(selectReposState);
+  useEffect(() => {
+    dispatch(fetchHomePageLoad());
+  }, [dispatch]);
 
-    return (
-        <Container>
-            <GitHub />
-            <Header>Portfolio</Header>
-            <SubTitle>My recent projects</SubTitle>
-        </Container>
-    );
+  return (
+    <Container>
+      <GitHub />
+      <Header>Portfolio</Header>
+      <SubTitle>My recent projects</SubTitle>
+
+      <StyledList>
+        {repositories.map((repo) => (
+          <StyledListItem key={repo.id}>
+            <SubHeader>{repo.name}</SubHeader>
+            <Description>{repo.description}</Description>
+            <Paragraph>
+              Code: <StyledLink href={repo.html_url}>{repo.html_url}</StyledLink>{" "}
+            </Paragraph>
+            <Paragraph>
+              Demo: <StyledLink href={repo.homepage}>{repo.homepage}</StyledLink>{" "}
+            </Paragraph>
+          </StyledListItem>
+        ))}
+      </StyledList>
+    </Container>
+  );
 };
--- src/feauters/personalHomepage/Portfolio/styled.js
+++ src/feauters/personalHomepage/Portfolio/styled.js
@@ -25,5 +25,41 @@ font-style: normal;
 font-weight: 400;
 line-height: 1.4; 
 letter-spacing: 1px;
-margin: 8px 0 0 0;
+margin: 8px 0 24px 0;
 `; 
+
+export const StyledList = styled.li`
+display: grid;
+grid-template-columns: 1fr 1fr;
+list-style: none;
+grid-gap:32px;
+`; 
+
+export const StyledListItem = styled.ul`
+padding: 56px;
+border-radius: 4px;
+border: 6px solid var(--diff-bg-neutral, rgba(209, 213, 218, 0.30));
+box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03), 0px -2px 50px 0px rgba(9, 10, 51, 0.02);
+background: ${({ theme }) => theme.section.background};
+margin: 0;
+display: flex;
+flex-direction: column;
+`; 
+
+export const SubHeader = styled.h2`
+color: ${({ theme }) => theme.primaryBlue.text};
+margin: 0;
+`; 
+
+export const StyledLink = styled.a`
+color: ${({ theme }) => theme.primaryBlue.text};
+margin: 0;
+`; 
+
+export const Paragraph = styled.span`
+margin: 8px 0 0 0;
+`;
+
+export const  Description = styled.p`
+margin:24px 0 16px 0;
+`;

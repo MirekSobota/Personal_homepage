0f5a94c82d4193d61c92a7b013d5fe6e1ee13ae0
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-06 21:44:54 +0200

    Added LinkedIn & GitHub & rename

---
+++ src/feauters/personalData.js
@@ -0,0 +1,3 @@
+export const email = "miroslaw.sobota@gmail.com";
+export const github = "https://github.com/MirekSobota";
+export const linkedin= "https://www.linkedin.com/in/mirek-sobota-7460b5211/;"

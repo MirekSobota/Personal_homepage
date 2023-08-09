3532f5269d6c3ef1cf598f3c56c625200337bb12
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-09 21:17:47 +0200

    Added new colors and dark theme

--- src/theme.js
+++ src/theme.js
@@ -2,9 +2,12 @@ const colorList = {
     white: "#FFFFFF",
     whiteLilac: "#FBFBFE",
     scienceBlue: "#0366D6",
+    DodgerBlue:"#2188FF",
     slateGray: "#6E7E91",
+    mercury: "#E5E5E5",
     iron: "#D1D5DA",
     mineShaft: "#252525",
+    grey:"#363636"
 };
 
 
@@ -29,5 +32,37 @@ export const lightTheme = {
         text: colorList.scienceBlue,
         background: colorList.scienceBlue,
     },
+    svg: {
+        fill: colorList.mineShaft,
+        hover: colorList.scienceBlue,
 
+    },
+
+};  
+
+export const darkTheme = {
+    primary: {
+        text: colorList.white,
+        background: colorList.mineShaft,
+    },
+    header: {
+        text: colorList.white,
+    },
+    white: {
+        text: colorList.white,
+    },
+    section:{
+        background: colorList.grey,
+    },
+    divider:{
+        background: colorList.mercury,
+    },
+    primaryBlue: {
+        text: colorList.scienceBlue,
+        background: colorList.scienceBlue,
+    },
+    svg: {
+        fill: colorList.white,
+        hover: colorList.DodgerBlue,
+    },
 };

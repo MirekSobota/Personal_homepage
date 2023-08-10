145190de9331cc7bdb0be4e016251b5fdc88d326
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-10 19:10:25 +0200

    Changed location & names

---
+++ src/features/personalHomepage/ThemeSwitcher/themeSwitchSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { lightTheme } from "../../../theme";
+
+const themeSwitchSlice = createSlice({
+  name: "themeSwitch",
+  initialState: {
+    theme: lightTheme,
+  },
+  reducers: {
+    toggleTheme: (state)=> {
+        state.theme = !state.theme;
+    },
+  },
+});
+
+
+export const {
+    toggleTheme,
+} = themeSwitchSlice.actions;
+
+export const selectThemeSwitchState = (state) => state.themeSwitch;
+export const selectTheme = (state) => selectThemeSwitchState(state).theme;
+
+export default  themeSwitchSlice.reducer;

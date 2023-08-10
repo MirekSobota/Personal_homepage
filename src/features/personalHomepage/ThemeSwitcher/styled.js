5b26faeeb9db4a3e6975f8e77fd0b531c02e1cff
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-10 19:16:43 +0200

    Added transition & colors to theme button

--- src/features/personalHomepage/ThemeSwitcher/styled.js
+++ src/features/personalHomepage/ThemeSwitcher/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+
 import { ReactComponent as SunIcon } from "../../../common/assets/sunIcon.svg";
 import { ReactComponent as BackgroundIcon } from "../../../common/assets/background.svg";
 import { ReactComponent as ToggleIcon } from "../../../common/assets/toggle.svg";
@@ -17,14 +18,14 @@ export const Wrapper = styled.div`
 `;
 
 export const Title = styled.p`
-font-size: 12px;
-font-style: normal;
-font-weight: 700;
-line-height: 1.3;
-text-transform: uppercase;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.3;
+  text-transform: uppercase;
 `;
 export const Button = styled.button`
- background-color: transparent;
+  background-color: transparent;
   border: none;
   cursor: pointer;
   width: 48px;
@@ -32,28 +33,34 @@ export const Button = styled.button`
 `;
 
 export const Background = styled(BackgroundIcon)`
-position: absolute;
+  position: absolute;
   width: 48px;
   height: 26px;
   transform: translate(-50%, -50%);
+  fill: ${({ theme }) => theme.svgTheme.fillBackGround};
 `;
 
 export const Toggle = styled(ToggleIcon)`
-position: absolute;
+  position: absolute;
   top: 50%;
   left: 50%;
   width: 20px;
   height: 20px;
-  transform: translate(-100%, -50%);
+  fill: ${({ theme }) => theme.svgTheme.fillToggle};
+  transition: transform 0.3s ease;
+  transform: ${props => props.$newProp ? 'translate(-100%, -50%)' : 'translate(0%, -50%)'};
 `;
 
 export const Sun = styled(SunIcon)`
-   position: absolute;
+  fill: ${({ theme }) => theme.svg.fill};
+  position: absolute;
   top: 50%;
   left: 50%;
   width: 14px;
   height: 14px;
-  transform: translate(-120%, -50%);
+  fill: ${({ theme }) => theme.svgTheme.fillSun};
+  transition: transform 0.3s ease;
+  transform: ${props => props.$newProp ? 'translate(-120%, -50%)' : 'translate(20%, -50%)'};
 `;
 
 

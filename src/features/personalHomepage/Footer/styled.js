f772596b59245eae11a9890887467ba8015c1e9c
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-10 20:10:20 +0200

    Added hover

--- src/features/personalHomepage/Footer/styled.js
+++ src/features/personalHomepage/Footer/styled.js
@@ -4,64 +4,67 @@ import { ReactComponent as GitHub } from "../../../common/assets/github.svg";
 import { ReactComponent as LinkedIn } from "../../../common/assets/linkedIn.svg";
 
 export const Container = styled.div`
-max-width: 700px;
-margin-top: 120px;
+  max-width: 700px;
+  margin-top: 120px;
 `;
 
 export const SubHeader = styled.p`
-font-weight: 700;
-text-transform: uppercase;
-font-size: 12px;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 12px;
 `;
 
 export const EmailButton = styled.a`
-color: ${({ theme }) => theme.header.text};
-font-size: 32px;
-font-style: normal;
-font-weight: 900;
-line-height: normal;
-letter-spacing: 1.6px;
-text-decoration-line: none;
-margin-top: 24px;
-transition: 0.2s ease;
+  color: ${({ theme }) => theme.header.text};
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  letter-spacing: 1.6px;
+  text-decoration-line: none;
+  margin-top: 24px;
+  transition: 0.2s ease;
 
 &:hover{
-    color: ${({ theme }) => theme.primaryBlue.text};  
+  color: ${({ theme }) => theme.primaryBlue.text};  
 }
 `;
 
 export const FooterText = styled.p`
-color: ${({ theme }) => theme.header.text};
-font-size: 18px;
-font-style: normal;
-font-weight: 400;
-line-height: 1.4;
-letter-spacing: 0.9px;
-margin-top: 24px;
+  color: ${({ theme }) => theme.header.text};
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.4;
+  letter-spacing: 0.9px;
+  margin-top: 24px;
 `;
 
 export const Wrapper = styled.div`
-display: flex;
-gap: 24px;
-margin-top: 56px;
+  display: flex;
+  gap: 24px;
+  margin-top: 56px;
 `;
 
 export const GitHubIcon = styled(GitHub)`
-transition: 0.2s ease;
-width: 48px;
-height: 48px;
-fill: ${({ theme }) => theme.svg.fill};
+  transition: 0.2s ease;
+  width: 48px;
+  height: 48px;
+  fill: ${({ theme }) => theme.svg.fill};
 
 &:hover{
-    fill: ${({ theme }) => theme.primaryBlue.background};  
+  fill: ${({ theme }) => theme.svg.hover};  
 }
 `;
 
 export const LinkedInIkon = styled(LinkedIn)`
-transition: 0.2s ease;
+  transition: 0.2s ease;
+  width: 48px;
+  height: 48px;
+  fill: ${({ theme }) => theme.svg.fill};
 
 &:hover{
-    fill: ${({ theme }) => theme.primaryBlue.background}; 
+  fill: ${({ theme }) => theme.svg.hover}; 
 }
 `;
 export const Link = styled.a`

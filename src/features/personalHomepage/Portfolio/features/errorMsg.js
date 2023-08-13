e489124930d4b6e5a9c200b0951b761977f40c19
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-13 21:38:26 +0200

    Added ErrorMessage component

---
+++ src/features/personalHomepage/Portfolio/features/errorMsg.js
@@ -0,0 +1,21 @@
+import { github } from "../../../../common/personalData";
+import { HireMeButton } from "../../Header/styled";
+import { ErrorIcon, SubHeader, LeadParagraph, StyledLink } from "./styled";
+
+export const ErrorMessage = () => {
+
+    return (
+        <>
+            <ErrorIcon />
+            <SubHeader>Ooops! Something went wrong... </SubHeader>
+            <LeadParagraph>Sorry, failed to load Github projects.
+                You can check them directly on Github.</LeadParagraph>
+            <HireMeButton>
+                <StyledLink href={github}>
+                    Go to Github
+                </StyledLink>
+            </HireMeButton>
+
+        </>
+    )
+};
--- src/features/personalHomepage/Portfolio/features/styled.js
+++ src/features/personalHomepage/Portfolio/features/styled.js
@@ -1,4 +1,5 @@
 import styled, { keyframes } from 'styled-components';
+import {ReactComponent as DangerIcon} from "../../../../common/assets/dangerIcon.svg";
 
 const spinAnimation = keyframes`
   0% { transform: rotate(0deg); }
@@ -14,7 +15,40 @@ export const Loader = styled.div`
   animation: ${spinAnimation} 1s linear infinite;  
 `;
 
+export const ErrorIcon = styled(DangerIcon)`
+stroke:  ${({ theme }) => theme.svg.fill};
+`;
 
+export const StyledLink = styled.a`
+  background-color: ${({ theme }) => theme.primaryBlue.background};
+  color: ${({ theme }) => theme.white.text};
+  padding: 12px 16px 12px 16px  ;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  text-decoration-line: none;
+`;
+
+export const SubHeader = styled.h3`
+color: ${({ theme }) => theme.header.text};
+font-size: 24px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+letter-spacing: 1.2px;
+margin:0;
+`;
+
+export const LeadParagraph = styled.p`
+color: ${({ theme }) => theme.header.text};
+font-size: 20px;
+font-style: normal;
+font-weight: 400;
+line-height: 1.4; 
+letter-spacing: 1px;
+margin:0;
+`;
 
 
 

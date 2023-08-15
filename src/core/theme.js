c0363514a32e246fe520c0195d4b66e078747fb2
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-15 23:08:12 +0200

    Working on colors

--- src/core/theme.js
+++ src/core/theme.js
@@ -1,97 +1,96 @@
 const colorList = {
-    white: "#FFFFFF",
-    whiteLilac: "#FBFBFE",
-    scienceBlue: "#0366D6",
-    dodgerBlue:"#2188FF",
-    slateGray: "#6E7E91",
-    mercury: "#E5E5E5",
-    iron: "#D1D5DA",
-    mineShaft: "#252525",
-    grey:"#363636", 
-    shadowDark:"#090a3308", 
-    shadowDarker: "#090a3305",
-    borderBlue: "#0366d633",
-    borderLightBlue: "#0366d680",
-    
-
+  white: "#FFFFFF",
+  whiteLilac: "#FBFBFE",
+  scienceBlue: "#0366D6",
+  dodgerBlue: "#2188FF",
+  slateGray: "#6E7E91",
+  mercury: "#E5E5E5",
+  iron: "#d2d6db4d",
+  mineShaft: "#252525",
+  grey: "#363636",
+  shadowDark: "#090a3308",
+  shadowDarker: "#090a3305",
+  tropicalBlue: "#CDE0F7",
+  azure: "#35669E",
+  tundora: "#414141",
 };
 
-
 export const lightTheme = {
-    primary: {
-        text: colorList.slateGray,
-        background: colorList.whiteLilac,
-    },
-    header: {
-        text: colorList.mineShaft,
-    },
-    white: {
-        text: colorList.white,
-    },
-    section:{
-        background: colorList.white,
-    },
-    divider:{
-        background: colorList.iron,
-    },
-    primaryBlue: {
-        text: colorList.scienceBlue,
-        background: colorList.scienceBlue,
-    },
-    svg: {
-        fill: colorList.mineShaft,
-        hover: colorList.scienceBlue,
-    },
-    svgTheme:{
-        fillBackGround: colorList.mercury,
-        fillToggle: colorList.slateGray,
-        fillSun: colorList.white,
-    },
-    shadow:{
-        colorOne: colorList.shadowDark,
-        colorTwo: colorList.shadowDarker,
-    },
-    border:{
-        borderLM:colorList.borderLightBlue,
-    }
-
-};  
+  primary: {
+    text: colorList.slateGray,
+    background: colorList.whiteLilac,
+  },
+  primaryBlue: {
+    text: colorList.scienceBlue,
+    background: colorList.scienceBlue,
+  },
+  header: {
+    text: colorList.mineShaft,
+  },
+  white: {
+    text: colorList.white,
+  },
+  section: {
+    background: colorList.white,
+  },
+  divider: {
+    background: colorList.iron,
+  },
+  svg: {
+    fill: colorList.mineShaft,
+    hover: colorList.scienceBlue,
+  },
+  svgTheme: {
+    fillBackGround: colorList.mercury,
+    fillToggle: colorList.slateGray,
+    fillSun: colorList.white,
+  },
+  shadow: {
+    colorOne: colorList.shadowDark,
+    colorTwo: colorList.shadowDarker,
+  },
+  border: {
+    border: colorList.iron,
+    borderHover: colorList.tropicalBlue,
+  },
+};
 
 export const darkTheme = {
-    primary: {
-        text: colorList.white,
-        background: colorList.mineShaft,
-    },
-    header: {
-        text: colorList.white,
-    },
-    white: {
-        text: colorList.white,
-    },
-    section:{
-        background: colorList.grey,
-    },
-    divider:{
-        background: colorList.mercury,
-    },
-    primaryBlue: {
-        text: colorList.dodgerBlue,
-        background: colorList.dodgerBlue,
-    },
-    svg: {
-        fill: colorList.white,
-        hover: colorList.dodgerBlue,
-    },
-    svgTheme:{
-        fillBackGround: colorList.slateGray,
-        fillToggle: colorList.white,
-        fillSun: colorList.mineShaft,
-    },
-    shadow:{
-        colorOne: colorList.shadowDark,
-        colorTwo: colorList.shadowDarker,
-    },
-    border:{
-        borderLM:colorList.borderBlue,
-    }
+  primary: {
+    text: colorList.white,
+    background: colorList.mineShaft,
+  },
+  primaryBlue: {
+    text: colorList.dodgerBlue,
+    background: colorList.dodgerBlue,
+  },
+  header: {
+    text: colorList.white,
+  },
+  white: {
+    text: colorList.white,
+  },
+  section: {
+    background: colorList.grey,
+  },
+  divider: {
+    background: colorList.mercury,
+  },
+  svg: {
+    fill: colorList.white,
+    hover: colorList.dodgerBlue,
+  },
+  svgTheme: {
+    fillBackGround: colorList.slateGray,
+    fillToggle: colorList.white,
+    fillSun: colorList.mineShaft,
+  },
+  shadow: {
+    colorOne: colorList.shadowDark,
+    colorTwo: colorList.shadowDarker,
+  },
+  border: {
+    border: colorList.tundora,
+    borderHover: colorList.azure,
+  },
 };

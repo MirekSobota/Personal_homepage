e45e98fd986b7feb9d8f3eb731ea3eb4d08a38bc
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-16 20:40:49 +0200

    Added new header text

--- src/features/personalHomepage/Header/index.js
+++ src/features/personalHomepage/Header/index.js
@@ -1,26 +1,41 @@
-import { Wrapper, About, Image, ThisIs, Name, Content, HireMeButton, HireMe, EmailIcon } from "./styled";
+import {
+  Wrapper,
+  About,
+  Image,
+  ThisIs,
+  Name,
+  Content,
+  HireMeButton,
+  HireMe,
+  EmailIcon,
+} from "./styled";
 import picture from "./images/mireksobota.jpg";
 import { email } from "../../../common/personalData";
 
-
 export const Header = () => {
-
-    return (
-        <>
-            <Wrapper>
-            <Image src={picture} alt="Mirek Sobota" />
-                <About>
-                    <ThisIs>This is</ThisIs>
-                    <Name>Mirek Sobota</Name>
-                    <Content>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Content>
-                    <HireMeButton >
-                        <HireMe href={`mailto:${email}`} title={email} rel="noopener noreferrer">
-                            <EmailIcon />
-                            HireMe
-                        </HireMe>
-                    </HireMeButton>
-                </About>
-            </Wrapper>
-        </>
-    )
+  return (
+    <>
+      <Wrapper>
+        <Image src={picture} alt="Mirek Sobota" />
+        <About>
+          <ThisIs>This is</ThisIs>
+          <Name>Mirek Sobota</Name>
+          <Content>
+            👨🏻‍💻 I’m a passionate Frontend Developer in love with React, currently
+            looking for new job opportunities.{" "}
+          </Content>
+          <HireMeButton>
+            <HireMe
+              href={`mailto:${email}`}
+              title={email}
+              rel="noopener noreferrer"
+            >
+              <EmailIcon />
+              HireMe
+            </HireMe>
+          </HireMeButton>
+        </About>
+      </Wrapper>
+    </>
+  );
 };

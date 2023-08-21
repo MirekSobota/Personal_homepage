2bd10eea33f1f91310cf1763577c069d393eda27
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-21 15:43:26 +0200

    Added sort

--- src/features/personalHomepage/Portfolio/getDataAPI.js
+++ src/features/personalHomepage/Portfolio/getDataAPI.js
@@ -4,6 +4,7 @@ const URL = " https://api.github.com/users";
 const user = "MirekSobota";
 
 export const getGitHubData = async () => {
-  const response = await axios.get(`${URL}/${user}/repos`);
+  const response = await axios.get(`${URL}/${user}/repos?sort=created`);
   return await response.data;
 };
+

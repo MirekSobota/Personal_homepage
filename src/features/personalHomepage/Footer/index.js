7a7f99115676d6e57a4d6cef229bb2b6ee21c02c
MirekSobota <miroslaw.sobota@gmail.com>
2023-08-22 21:52:55 +0200

    Added emoticon to footer

--- src/features/personalHomepage/Footer/index.js
+++ src/features/personalHomepage/Footer/index.js
@@ -12,7 +12,7 @@ export const Footer = () => {
             <Container>
                 <SubHeader>Let’s talk!</SubHeader>
                 <EmailButton href={`mailto:${email}`} title={email} rel="noopener noreferrer">miroslaw.sobota@gmail.com</EmailButton>
-                <FooterText>I’m always open to new projects whenever I have the time. If you have a website or dashboard in mind and need some help to make your ideas come to life, feel free to conatct me </FooterText>
+                <FooterText>I’m always open to new projects whenever I have the time. If you have a website or dashboard in mind and need some help to make your ideas come to life, feel free to contact me ✌ </FooterText>
                 <Wrapper>
                     <Link href={github}>
                     <GitHubIcon />
